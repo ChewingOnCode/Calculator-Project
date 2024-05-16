a36b050449cb2f5c91a9bdbe6e050609117e68a2
ChewingOnCode <109337208+ChewingOnCode@users.noreply.github.com>
2024-05-16 03:06:35 -0400

    Updated JS Logic

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let buffer = "0";
+let runningTotal = 0;
+let previousOperator = null;
 const screen = document.querySelector(".screen");
 function buttonClick(value) {
   if (isNaN(parseInt(value))) {
@@ -17,8 +19,62 @@ function handleNumber(number) {
   }
 }
 
+function handleMath(value) {
+  if (buffer === "0") {
+    // do nothing
+    return;
+  }
+  const intBuffer = parseInt(buffer);
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+  previousOperator = value;
+  buffer = "0";
+  console.log(runningTotal);
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "-") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "x") {
+    runningTotal *= intBuffer;
+  } else if (previousOperator === "÷") {
+    runningTotal /= intBuffer;
+  }
+}
+
 function handleSymbol(symbol) {
-    
+  switch (symbol) {
+    case "C":
+      buffer = "0";
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = "" + runningTotal;
+      runningTotal = 0;
+      break;
+    case "&larrow;":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "+":
+    case "-":
+    case "x":
+    case "÷":
+      handleMath(symbol);
+      break;
+  }
 }
 
 function init() {
